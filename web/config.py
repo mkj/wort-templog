0cfaa1b5e8921a5e0a05bb5ef40f8154fefd7cbc
Matt Johnston <matt@ucc.asn.au>
2012-06-13 23:41:05 +0800

    - Various fixes for web server, kind of works

--- web/config.py
+++ web/config.py
@@ -4,9 +4,12 @@ DATA_PATH = 'data'
 HMAC_KEY = 'a hmac key'
 
 GRAPH_WIDTH = 800
+GRAPH_HEIGHT = 400
 
 LINE_WIDTH = 2
 
-SENSOR_NAMES = {}
+SENSOR_NAMES = {'sensor_test1': "First Test"}
 
 SENSOR_COLOURS = {}
+
+GRAPH_FONT = "Courier"
