02646b0555d23520d5a6bfafa4665b2d9144d84d
Matt Johnston <matt@ucc.asn.au>
2012-07-07 23:32:02 +0800

    ui tweaks

--- web/config.py
+++ web/config.py
@@ -5,20 +5,22 @@ HMAC_KEY = 'a hmac key'
 
 UPDATE_URL = 'https://evil.ucc.asn.au/~matt/templog/update'
 
-GRAPH_WIDTH = 800
-GRAPH_HEIGHT = 400
-ZOOM = 1.5
+GRAPH_WIDTH = 1200
+GRAPH_HEIGHT = 600
+ZOOM = 1
 
 LINE_WIDTH = 2
 
 SENSOR_NAMES = {'sensor_28 CE B2 1A 03 00 00 99': "Wort",
-    'sensor_28 CC C1 1A 03 00 00 D4': "Lid",
+    'sensor_28 CC C1 1A 03 00 00 D4': "Ambient",
+    'sensor_28 49 BC 1A 03 00 00 54': "Other",
     'sensor_voltage': 'Voltage',
     }
 
 SENSOR_COLOURS = {'Wort': 'e49222', 
-                'Lid': '028b3d',
+                'Ambient': '028b3d',
                 'Voltage': '7db5d3aa',
+                'Other': '78000c',
                 }
 
 
