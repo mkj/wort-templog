7d239eb6b38f2774df01b83797187addbb322129
Matt Johnston <matt@ucc.asn.au>
2012-07-16 22:01:33 +0800

    fix rem calculation

--- web/log.py
+++ web/log.py
@@ -160,7 +160,7 @@ def convert_ds18b20_12bit(reading):
 
 def time_rem(name, entries):
     val_ticks = int(entries[name])
-    val_rem = int(entries[name])
+    val_rem = int(entries['%s_rem' % name])
     tick_wake = int(entries['tick_wake'])
     tick_secs = int(entries['tick_secs'])
     return val_ticks + float(val_rem) * tick_secs / tick_wake
