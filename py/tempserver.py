f66a9bfce3f9a2e122c37e63bce25e4beec89bef
Matt Johnston <matt@ucc.asn.au>
2013-01-12 21:54:55 +0800

    less verbose logging

--- py/tempserver.py
+++ py/tempserver.py
@@ -94,7 +94,7 @@ class Tempserver(object):
 def setup_logging():
     logging.basicConfig(format='%(asctime)s %(message)s', 
             datefmt='%m/%d/%Y %I:%M:%S %p',
-            level=logging.DEBUG)
+            level=logging.INFO)
 
 def start():
     with Tempserver() as server:
