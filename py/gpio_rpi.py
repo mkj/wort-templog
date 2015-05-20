cb3e93359a55375e75ea086c9c8cc8306ea29e05
Matt Johnston <matt@ucc.asn.au>
2015-05-20 23:10:37 +0800

    fix gpio

--- py/gpio_rpi.py
+++ py/gpio_rpi.py
@@ -7,15 +7,43 @@ from utils import L,D,EX,W
 __all__ = ["Gpio"]
 
 class Gpio(object):
-	def __init__(self, pin, name):
-		self.pin = pin
-		self.name = name
-		GPIO.setmode(GPIO.BOARD)
-		GPIO.setup(self.pin, GPIO.OUT)
+    SYS_GPIO_BASE = '/sys/class/gpio/gpio'
+    def __init__(self, pin, name):
+        self.pin = pin
+        self.name = name
 
-	def turn(self, value):
-		self.state = bool(value)
-		GPIO.output(self.pin, self.state)
+        dir_fn = '%s%d/direction' % (self.SYS_GPIO_BASE, pin)
+        with open(dir_fn, 'w') as f:
+            # make sure it doesn't start "on"
+            f.write('low')
+        val_fn = '%s%d/value' % (self.SYS_GPIO_BASE, pin)
+        self.value_file = open(val_fn, 'r+')
 
-	def get_state(self):
-		return GPIO.input(self.pin)
+    def turn(self, value):
+        self.value_file.seek(0)
+        self.value_file.write('1' if value else '0')
+        self.value_file.flush()
+
+    def get_state(self):
+        self.value_file.seek(0)
+        buf = self.value_file.read().strip()
+        if buf == '0':
+            return False
+        if buf != '1':
+            E("Bad value read from gpio '%s': '%s'" 
+                % (self.value_file.name, buf))
+        return True
+
+
+def main():
+    g = Gpio(17, 'f')
+    g.turn(1)
+
+    print(g.get_state())
+
+    g.turn(0)
+
+    print(g.get_state())
+
+if __name__ == '__main__':
+    main()
