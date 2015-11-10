5777b1ef8fe918982bad931813ee171f19c2a7b9
Matt Johnston <matt@ucc.asn.au>
2015-11-10 21:10:57 +0800

    chown gpio files too

--- py/setup_gpio.sh
+++ py/setup_gpio.sh
@@ -2,6 +2,9 @@
 
 # this must run as root
 
+chgrp gpio /sys/class/gpio/gpio17/direction /sys/class/gpio/gpio17/value
+chmod g+w /sys/class/gpio/gpio17/direction /sys/class/gpio/gpio17/value
+
 PINS="17"
 
 for PIN in $PINS; do
