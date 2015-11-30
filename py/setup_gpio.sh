1da905fba6af1d27075ed08e3d1835b60afe6020
Matt Johnston <matt@ucc.asn.au>
2015-11-30 21:40:45 +0800

    fix gpio chown script

--- py/setup_gpio.sh
+++ py/setup_gpio.sh
@@ -2,11 +2,12 @@
 
 # this must run as root
 
-chgrp gpio /sys/class/gpio/gpio17/direction /sys/class/gpio/gpio17/value
-chmod g+w /sys/class/gpio/gpio17/direction /sys/class/gpio/gpio17/value
 
 PINS="17"
 
 for PIN in $PINS; do
     echo $PIN > /sys/class/gpio/export
 done
+
+chgrp gpio /sys/class/gpio/gpio17/direction /sys/class/gpio/gpio17/value
+chmod g+w /sys/class/gpio/gpio17/direction /sys/class/gpio/gpio17/value
